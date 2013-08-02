d6f6658c2fcb5a40ea8f27850f4f5679e6b40795
masayuki miyauchi <miyauchi0623@gmail.com>
2013-08-02 18:31:04 +0900

    JSHint対応

--- web-study/src/main/webapp/resources/js/src/sample.js
+++ web-study/src/main/webapp/resources/js/src/sample.js
@@ -1,83 +1,85 @@
+/* global webstudy */
 $(function() {
-    var db = webstudy.sampleDB;
-    var util = webstudy.util;
-    db.open();
+	"use strict";
+	var db = webstudy.sampleDB;
+	var util = webstudy.util;
+	db.open();
 
-    /**
-     * 結果領域をクリアする.
-     */
-    function clearResult() {
-	var $result = $("#result ul");
-	$result.empty();
-    }
-    /**
-     * 結果領域にDOMを追加する.
-     * 
-     * @param {string} text 追加するテキスト
-     */
-    function appendResult(text) {
-	var $result = $("#result ul");
-	var $target = $("<li>");
-	$target.text(text).appendTo($result);
-    }
-    /**
-     * ストレージに値を保存する.
-     * 
-     * @param {sessionStrage|localStorage} storage
-     */
-    function appendStrage(storage) {
-	var value = storage.getItem("key") || 0;
-	value++;
-	storage.setItem("key", value++);
-    }
-
-    // イベントハンドラ
-
-    $("#button-click-id").click(function() {
-	console.log("#button-clicked!");
-	appendResult("Clicked");
-    });
-
-    $("#button-clear-id").click(function() {
-	console.log("#clear-clicked!");
-	clearResult();
-	$.removeCookie("key", null);
-	localStorage.removeItem("key");
-	sessionStorage.removeItem("key");
-    });
-
-    $("#button-getJson-id").click(function() {
-	console.log("#getJson-clicked!");
-	var someJson = util.getJson();
-	appendResult(someJson.someString);
-	appendResult(someJson.someFunction());
-    });
-
-    $("#button-throwException-id").click(function() {
-	console.log("#throwException-clicked!");
-	try {
-	    throw "throw Exception!!";
-	} catch (e) {
-	    throw "Rethrow Exception!!";
+	/**
+	 * 結果領域をクリアする.
+	 */
+	function clearResult() {
+		var $result = $("#result ul");
+		$result.empty();
 	}
-    });
+	/**
+	 * 結果領域にDOMを追加する.
+	 * 
+	 * @param {string} text 追加するテキスト
+	 */
+	function appendResult(text) {
+		var $result = $("#result ul");
+		var $target = $("<li>");
+		$target.text(text).appendTo($result);
+	}
+	/**
+	 * ストレージに値を保存する.
+	 * 
+	 * @param {sessionStrage|localStorage} storage
+	 */
+	function appendStrage(storage) {
+		var value = storage.getItem("key") || 0;
+		value++;
+		storage.setItem("key", value++);
+	}
 
-    $("#button-appendLocalStorage-id").click(function() {
-	appendStrage(localStorage);
-	appendResult("ローカルストレージに保存!");
-    });
+	// イベントハンドラ
 
-    $("#button-appendSessionStorage-id").click(function() {
-	appendStrage(sessionStorage);
-	appendResult("セッションストレージに保存!");
-    });
+	$("#button-click-id").click(function() {
+		console.log("#button-clicked!");
+		appendResult("Clicked");
+	});
 
-    $("#button-appendCookie-id").click(function() {
-	var strage = {
-	    getItem : $.cookie,
-	    setItem : $.cookie
-	};
-	appendStrage(strage);
-	appendResult("クッキーを保存!");
-    });
+	$("#button-clear-id").click(function() {
+		console.log("#clear-clicked!");
+		clearResult();
+		$.removeCookie("key", null);
+		localStorage.removeItem("key");
+		sessionStorage.removeItem("key");
+	});
+
+	$("#button-getJson-id").click(function() {
+		console.log("#getJson-clicked!");
+		var someJson = util.getJson();
+		appendResult(someJson.someString);
+		appendResult(someJson.someFunction());
+	});
+
+	$("#button-throwException-id").click(function() {
+		console.log("#throwException-clicked!");
+		try {
+			throw "throw Exception!!";
+		} catch (e) {
+			throw "Rethrow Exception!!";
+		}
+	});
+
+	$("#button-appendLocalStorage-id").click(function() {
+		appendStrage(localStorage);
+		appendResult("ローカルストレージに保存!");
+	});
+
+	$("#button-appendSessionStorage-id").click(function() {
+		appendStrage(sessionStorage);
+		appendResult("セッションストレージに保存!");
+	});
+
+	$("#button-appendCookie-id").click(function() {
+		var strage = {
+			getItem : $.cookie,
+			setItem : $.cookie
+		};
+		appendStrage(strage);
+		appendResult("クッキーを保存!");
+	});
 });
--- web-study/src/main/webapp/resources/js/src/util.js
+++ web-study/src/main/webapp/resources/js/src/util.js
@@ -1,21 +1,22 @@
 (function(window) {
+	"use strict";
 
-    var webstudy = window.webstudy;
-    if (webstudy === undefined) {
-	webstudy = {};
-    }
-    webstudy.util = {
-	getJson : function() {
-	    return {
-		someBoolean : true,
-		someString : "someString",
-		someNumber : "someNumber",
-		someFunction : function() {
-		    return "invokeFunction";
-		}
-	    };
+	var webstudy = window.webstudy;
+	if (webstudy === undefined) {
+		webstudy = {};
 	}
-    };
+	webstudy.util = {
+		getJson : function() {
+			return {
+				someBoolean : true,
+				someString : "someString",
+				someNumber : "someNumber",
+				someFunction : function() {
+					return "invokeFunction";
+				}
+			};
+		}
+	};
 
-    window.webstudy = webstudy;
+	window.webstudy = webstudy;
 })(window);
