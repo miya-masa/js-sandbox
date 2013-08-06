c422c8974dae2b932fd44fe5b7f62af85721b597
masayuki miyauchi <miyauchi0623@gmail.com>
2013-08-06 18:40:47 +0900

    テスト関連の資料追加

--- js-sandbox/src/main/webapp/resources/js/test/scope-test.js
+++ js-sandbox/src/main/webapp/resources/js/test/scope-test.js
@@ -1,98 +1,98 @@
 (function() {
-    "use strict";
+	"use strict";
 
-    var log = myms.log;
+	var log = myms.log;
 
-    test(
-	    "スコープの確認",
-	    function() {
-		
-		ok(true, "グローバルエリアスタート========================================");
-		ok(typeof outer === 'function', "outer()はスコープの中");
-		ok(typeof inner === 'function', "inner()はスコープの中");
-		ok(typeof a === 'number', "aはスコープの中");
-		ok(typeof b === 'number', "bはスコープの中");
-		ok(typeof c === 'number', "cはスコープの中");
+	test(
+			"スコープの確認",
+			function() {
 
-		function outer() {
-		    ok(true,
-			    "outer関数スタート========================================");
+				ok(true, "グローバルエリアスタート========================================");
+				ok(typeof outer === 'function', "outer()はスコープの中");
+				ok(typeof inner === 'function', "inner()はスコープの中");
+				ok(typeof a === 'number', "aはスコープの中");
+				ok(typeof b === 'number', "bはスコープの中");
+				ok(typeof c === 'number', "cはスコープの中");
 
-		    ok(typeof outer === 'function', "outer()はスコープの中");
-		    ok(typeof inner === 'function', "inner()はスコープの中");
-		    ok(typeof a === 'number', "aはスコープの中");
-		    ok(typeof b === 'number', "bはスコープの中");
-		    ok(typeof c === 'number', "cはスコープの中");
+				function outer() {
+					ok(true,
+							"outer関数スタート========================================");
 
-		    var a = 10;
+					ok(typeof outer === 'function', "outer()はスコープの中");
+					ok(typeof inner === 'function', "inner()はスコープの中");
+					ok(typeof a === 'number', "aはスコープの中");
+					ok(typeof b === 'number', "bはスコープの中");
+					ok(typeof c === 'number', "cはスコープの中");
 
-		    ok(true,
-			    "outer関数内 var aを定義直後========================================");
-		    ok(typeof outer === 'function', "outer()はスコープの中");
-		    ok(typeof inner === 'function', "inner()はスコープの中");
-		    ok(typeof a === 'number', "aはスコープの中");
-		    ok(typeof b === 'number', "bはスコープの中");
-		    ok(typeof c === 'number', "cはスコープの中");
+					var a = 10;
 
-		    function inner() {
-			ok(true,
-				"inner関数スタート========================================");
-			ok(typeof outer === 'function', "outer()はスコープの中");
-			ok(typeof inner === 'function', "inner()はスコープの中");
-			ok(typeof a === 'number', "aはスコープの中");
-			ok(typeof b === 'number', "bはスコープの中");
-			ok(typeof c === 'number', "cはスコープの中");
+					ok(true,
+							"outer関数内 var aを定義直後========================================");
+					ok(typeof outer === 'function', "outer()はスコープの中");
+					ok(typeof inner === 'function', "inner()はスコープの中");
+					ok(typeof a === 'number', "aはスコープの中");
+					ok(typeof b === 'number', "bはスコープの中");
+					ok(typeof c === 'number', "cはスコープの中");
 
-			var b = 20;
-			ok(true,
-				"inner関数内 var bを定義直後========================================");
-			ok(typeof outer === 'function', "outer()はスコープの中");
-			ok(typeof inner === 'function', "inner()はスコープの中");
-			ok(typeof a === 'number', "aはスコープの中");
-			ok(typeof b === 'number', "bはスコープの中");
-			ok(typeof c === 'number', "cはスコープの中");
+					function inner() {
+						ok(true,
+								"inner関数スタート========================================");
+						ok(typeof outer === 'function', "outer()はスコープの中");
+						ok(typeof inner === 'function', "inner()はスコープの中");
+						ok(typeof a === 'number', "aはスコープの中");
+						ok(typeof b === 'number', "bはスコープの中");
+						ok(typeof c === 'number', "cはスコープの中");
 
-			if (a === 10) {
-			    ok(true,
-				    "inner関数内 ifブロックスタート========================================");
-			    ok(typeof outer === 'function', "outer()はスコープの中");
-			    ok(typeof inner === 'function', "inner()はスコープの中");
-			    ok(typeof a === 'number', "aはスコープの中");
-			    ok(typeof b === 'number', "bはスコープの中");
-			    ok(typeof c === 'number', "cはスコープの中");
-			    var c = 30;
-			    ok(true,
-				    "inner関数内 ifブロック内var c定義直後========================================");
-			    ok(typeof outer === 'function', "outer()はスコープの中");
-			    ok(typeof inner === 'function', "inner()はスコープの中");
-			    ok(typeof a === 'number', "aはスコープの中");
-			    ok(typeof b === 'number', "bはスコープの中");
-			    ok(typeof c === 'number', "cはスコープの中");
-			}
-			ok(true,
-				"inner関数内 ifブロック終了直後========================================");
-			ok(typeof outer === 'function', "outer()はスコープの中");
-			ok(typeof inner === 'function', "inner()はスコープの中");
-			ok(typeof a === 'number', "aはスコープの中");
-			ok(typeof b === 'number', "bはスコープの中");
-			ok(typeof c === 'number', "cはスコープの中");
-		    }
-		    inner();
-		    ok(true,
-			    "inner関数 終了直後========================================");
-		    ok(typeof outer === 'function', "outer()はスコープの中");
-		    ok(typeof inner === 'function', "inner()はスコープの中");
-		    ok(typeof a === 'number', "aはスコープの中");
-		    ok(typeof b === 'number', "bはスコープの中");
-		    ok(typeof c === 'number', "cはスコープの中");
-		}
-		outer();
-		ok(true, "outer関数 終了直後========================================");
-		ok(typeof outer === 'function', "outer()はスコープの中");
-		ok(typeof inner === 'function', "inner()はスコープの中");
-		ok(typeof a === 'number', "aはスコープの中");
-		ok(typeof b === 'number', "bはスコープの中");
-		ok(typeof c === 'number', "cはスコープの中");
-	    });
+						var b = 20;
+						ok(true,
+								"inner関数内 var bを定義直後========================================");
+						ok(typeof outer === 'function', "outer()はスコープの中");
+						ok(typeof inner === 'function', "inner()はスコープの中");
+						ok(typeof a === 'number', "aはスコープの中");
+						ok(typeof b === 'number', "bはスコープの中");
+						ok(typeof c === 'number', "cはスコープの中");
+
+						if (a === 10) {
+							ok(true,
+									"inner関数内 ifブロックスタート========================================");
+							ok(typeof outer === 'function', "outer()はスコープの中");
+							ok(typeof inner === 'function', "inner()はスコープの中");
+							ok(typeof a === 'number', "aはスコープの中");
+							ok(typeof b === 'number', "bはスコープの中");
+							ok(typeof c === 'number', "cはスコープの中");
+							var c = 30;
+							ok(true,
+									"inner関数内 ifブロック内var c定義直後========================================");
+							ok(typeof outer === 'function', "outer()はスコープの中");
+							ok(typeof inner === 'function', "inner()はスコープの中");
+							ok(typeof a === 'number', "aはスコープの中");
+							ok(typeof b === 'number', "bはスコープの中");
+							ok(typeof c === 'number', "cはスコープの中");
+						}
+						ok(true,
+								"inner関数内 ifブロック終了直後========================================");
+						ok(typeof outer === 'function', "outer()はスコープの中");
+						ok(typeof inner === 'function', "inner()はスコープの中");
+						ok(typeof a === 'number', "aはスコープの中");
+						ok(typeof b === 'number', "bはスコープの中");
+						ok(typeof c === 'number', "cはスコープの中");
+					}
+					inner();
+					ok(true,
+							"inner関数 終了直後========================================");
+					ok(typeof outer === 'function', "outer()はスコープの中");
+					ok(typeof inner === 'function', "inner()はスコープの中");
+					ok(typeof a === 'number', "aはスコープの中");
+					ok(typeof b === 'number', "bはスコープの中");
+					ok(typeof c === 'number', "cはスコープの中");
+				}
+				outer();
+				ok(true, "outer関数 終了直後========================================");
+				ok(typeof outer === 'function', "outer()はスコープの中");
+				ok(typeof inner === 'function', "inner()はスコープの中");
+				ok(typeof a === 'number', "aはスコープの中");
+				ok(typeof b === 'number', "bはスコープの中");
+				ok(typeof c === 'number', "cはスコープの中");
+			});
 
 })();
